3915f214d3156b8d77d1c2ac9482157776094264
Bhupendra Pandit <138773922+Bhu559910@users.noreply.github.com>
2023-09-02 08:23:41 +0530

    third_commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB = 'mongodb+srv://panditbhupendra219:PtZxBRqHTex56qoR@cluster0.ggysf2a.mongodb.net/?retryWrites=true&w=majority';
+const DB = 'mongodb+srv://panditbhupendra219:tN8lLkIZtGCylWBS@cluster0.0cbg3wd.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(DB, {
 	useNewUrlParser: true,
